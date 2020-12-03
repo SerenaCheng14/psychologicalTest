0e63f41c9ce148f501b968c466203a4cd46ec607
SerenaCheng <ccc9514789@gmail.com>
2020-12-03 13:52:52 +0800

    Initial Commit

---
+++ innateGenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  innateGenger
+//
+//  Created by Damon on 2020/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
